8742b252c70f11e3108d4ab7d74ce79f6fc9f6ec
Mohammed <dev.mohammedsh@gmail.com>
2024-04-23 13:32:34 +0530

    Add nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,24 @@
+--
+-- ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+-- ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+-- ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+-- ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+-- ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+-- ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+--
+-- Neovim Lua Config File by Mohammed
+-- INIT FILE
+
+-- Variables
+opt = vim.opt
+g = vim.g
+kmap = vim.keymap
+cmd = vim.cmd
+
+-- Leader Key
+g.mapleader = " "
+
+-- Imports
+require("core.lazy")                -- lazy.nvim plugin manager
+require("core.settings")            -- Editor settings
+require("core.mappings")            -- Mappings
